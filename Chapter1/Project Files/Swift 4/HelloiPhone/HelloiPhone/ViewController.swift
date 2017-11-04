8d247b596e4b8fa45a5f4166438218cb9689ce38
Hakyung Noh <hakyung1992@gmail.com>
2017-11-05 00:20:14 +0900

    2. HelloiPhone(Xcode 9)
    Chapter 1 - HelloiPhone

---
+++ Chapter1/Project Files/Swift 4/HelloiPhone/HelloiPhone/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  HelloiPhone
+//
+//  Created by Hakyung Noa on 05/11/2017.
+//  Copyright © 2017 Hakyung Noah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var myLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func change(_ sender: Any) {
+        myLabel.text = "Hello World"
+    }
+    
+}
+
